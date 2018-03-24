8d6be36b5f51863d7d51fd4749650eacb309976a
luisafvaca <luisafernanda9111@gmail.com>
2018-03-24 18:04:59 -0500

    added data in the internal state

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import createReactClass from 'create-react-class';
 import Controller from './components/generator';
 
 var App = createReactClass({
+  getInitialState: function(){
+   return {
+     color: 0
+   }
+  },
+  updateColor: function(e){
+    this.setState({
+      color: e.target.value
+    })
+  },
   render: function(){
-    console.log(Controller, '----------');
+    console.log(this.state);
     return (
-      <Controller />
+      <section>
+        <Controller updateColor={this.updateColor}/>
+      </section>
     )
   }
 })
