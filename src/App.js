4847efd53cd2212f886cbdacea3e0a384929e592
luisafvaca <luisafernanda9111@gmail.com>
2018-03-24 17:53:46 -0500

    console.log of input data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import createReactClass from 'create-react-class';
 import Controller from './components/generator';
 
--- src/components/generator.js
+++ src/components/generator.js
@@ -2,12 +2,16 @@ import React from "react";
 import createReactClass from 'create-react-class';
 
 var Controller = createReactClass({
+    update: function(e){
+        console.log(e.target.value)
+    },
     render: function(){
         var inputPropierties = {
             type: 'range',
             min: '0',
             max: '255',
-            defaultValue: 255
+            defaultValue: 255,
+            onChange: this.update
         }
         return (
             React.createElement('input', inputPropierties)
